4e93757f6d7a1ff63982747c3458421aa034d002
LijKal <getkalnow@gmail.com>
2023-04-26 16:17:33 +0300

    TX_PREV Numerator created

--- api/src/main/java/org/openmrs/module/ohrireports/OHRIReportsConstants.java
+++ api/src/main/java/org/openmrs/module/ohrireports/OHRIReportsConstants.java
@@ -156,6 +156,8 @@ public class OHRIReportsConstants {
 	public final static String CXCA_TYPE_OF_SCREENING_RESCREENING = "13c3ee77-4e7c-4224-ae40-0b2727932a0f";
 	
 	public final static String CXCA_TYPE_OF_SCREENING_POST_TREATMENT = "3f4a6148-39c1-4980-81c6-6d703367c4a6";
+
+	public final static String TPT_START_DATE = "162320AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
 	
 	/**
 	 * Reports
