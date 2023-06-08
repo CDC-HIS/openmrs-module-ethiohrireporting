2e6eb4fa0e30444a0a3e9f92e5dfa53106c9e279
abdisha <asresabdi@gmail.com>
2023-06-08 09:18:14 +0300

    hmis head and list of columns created as constant in HMISconstant

---
+++ api/src/main/java/org/openmrs/module/ohrireports/reports/datasetevaluator/hmis/HMISConstant.java
@@ -0,0 +1,9 @@
+package org.openmrs.module.ohrireports.reports.datasetevaluator.hmis;
+
+public class HMISConstant {
+    public final static String HEADER_NAME ="06 - HIV | Hospital, Health center, Clinic | Monthly (Federal Ministry Of Health)";
+    public final static String COLUMN_1_NAME = "S.NO";
+    public final static String COLUMN_2_NAME = "Activity";
+    public final static String COLUMN_4_NAME = "Follow-up regimen";
+    public final static String COLUMN_5_NAME = "Follow-up Index";
+}
