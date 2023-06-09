17f38393c0796e9ed7c771f3efe877326d612dc9
LijKal <getkalnow@gmail.com>
2023-06-09 14:20:21 +0300

    hmis tx curr list of regimen added

--- api/src/main/java/org/openmrs/module/ohrireports/OHRIReportsConstants.java
+++ api/src/main/java/org/openmrs/module/ohrireports/OHRIReportsConstants.java
@@ -272,4 +272,63 @@ public class OHRIReportsConstants {
 	public final static String R1d_AZT_3TC_EFV = "b5951dd9-6bb2-4b63-af20-0707500108ea";
 	public final static String R1e_TDF_3TC_EFV = "1f735bdd-4ccd-40ce-8746-28f031f38675";
 	public final static String R1f_TDF_3TC_NVP = "8b727274-4a3c-4a81-ae50-9f759a87358d";
+
+	public final static String R1g_ABC_3TC_EFV = "a664a63f-57f5-4cfc-80c0-4a8456725c2a";
+	public final static String R1h_ABC_3TC_NVP = "71ecf102-8d3d-4a2a-b799-30773ad5d447";
+	public final static String R1j_TDF_3TC_DTG = "2a811ceb-3c34-47ff-943a-425b41651784";
+	public final static String R1k_AZT_3TC_DTG = "c56be3a9-d678-4693-9566-84430239a2f5";
+
+	public final static String R2a_ABC_DDL_LPV = "a87d9674-11af-4440-afd0-d097d84d0393";
+	public final static String R2b_ABC_DDL_NFV = "82e23d8c-4c9d-4b18-a960-9f8890b86f2f";
+	public final static String R2c_TDF_DDL_LPV = "cf29c24b-b796-4536-aaa5-8303f45bb5f8";
+	public final static String R2d_TDF_DDL_NFV = "2e84a82a-8b93-40fc-a556-2028d39f5710";
+
+	public final static String R2e_AZT_3TC_LPVr = "a1056dfb-c16d-4e5c-a56b-e33228d634f8";
+	public final static String R2f_AZT_3TC_ATVr = "891bf06e-298a-40ee-8970-2311d18fed48";
+	public final static String R2g_TDF_3TC_LPVr = "e2d1786e-4849-473e-b32a-2c82bd35e947";
+	public final static String R2h_TDF_3TC_ATVr = "2e84a82a-8b93-40fc-a556-2028d39f5710";
+
+	public final static String R2i_ABC_3TC_LPVr = "73c10f0d-0a24-411f-9c97-49ab9e7eb2ce";
+	public final static String R2k_AZT_3TC_DTG = "707d6943-9ec0-4c6b-8c72-f64143663043";
+
+	public final static String R3c_DRVr_ABC_3TC_DTG = "664137bb-d538-4afc-bbd1-7538cf158882";
+	public final static String R3e_DRVr_TDF_3TC_EFV = "74f685e8-7ea1-40e1-adf1-651cdf30b272";
+	public final static String R3f_DRVr_AZT_3TC_EFV = "e8cdf6eb-2b36-4cde-bb4b-f0fd1d6d748b";
+
+	public final static String R4a_D4T_3TC_NVP = "c33efc4a-734f-4e89-92a4-71b259a5e547";
+	public final static String R4b_D4T_3TC_EFV = "7bbee32a-31a8-4199-a8a2-2d15c748d80a";
+	public final static String R4c_AZT_3TC_NVP = "756b4487-f358-4817-b248-9c05080bad67";
+	public final static String R4d_AZT_3TC_EFV = "ad169a6d-cf1a-4363-a2da-be85998612bd";
+
+	public final static String R4e_TDF_3TC_EFV = "d3eedcf3-9571-42a2-b8b0-189e93f72f47";
+	public final static String R4f_AZT_3TC_LPVr = "0343175a-e931-4f6c-a0c2-99e55637bda9";
+	public final static String R4g_ABC_3TC_LPVr = "e4353cd1-5a0e-4870-8af4-f012ca0145fe";
+
+	public final static String R4i_TDF_3TC_DTG = "0dbe6b49-7356-4cf0-b489-890e18c38465";
+	public final static String R4j_ABC_3TC_DTG = "eae01634-65ec-4174-a95c-622456663727";
+	public final static String R4L_ABC_3TC_EFV = "c94ca2da-8066-42f7-b4c9-582dae83e552";
+
+	public final static String R5a_ABC_DDL_LPY = "fcafc964-f018-41c8-9dc9-343a2f4a94b5";
+	public final static String R5b_ABC_DDL_NFV = "7c30ed1a-a770-4b74-8704-82077427128d";
+	public final static String R5c_TDF_DDL_LPV = "18c78c5f-082c-4acc-a121-62fa334f34d3";
+	public final static String R5d_TDF_DDL_NFV = "5c9424e6-4f18-44e1-a074-e561643a7d91";
+
+	public final static String R5e_ABC_3TC_LPVr = "fbb4db0f-4cf4-4ca3-bde0-e20eb065e7da";
+	public final static String R5f_AZT_3TC_LPVr = "9ef4d67d-0e05-422a-aac5-80eaf71daadc";
+	public final static String R5g_TDF_3TC_EFV = "ee5b1c56-1131-40a6-a880-5dd33a72a861";
+	public final static String R5h_ABC_3TC_EFV = "e341cf6c-4923-4eb1-8c47-e76eef19e8cf";
+	
+	public final static String R5i_TDF_3TC_LPVr = "1294646b-2c36-4169-a0aa-a0c7692003fc";
+	public final static String R5k_RAL_AZT_3TC = "2e58ef33-d019-4bd9-97b8-553e39e12646";
+	public final static String R5L_RAL_ABC_3TC = "8feb33db-71db-4fd0-a0e7-c02f81fcc76f";
+
+	public final static String R6a_DRVr_RAL_AZT_3TC = "5415f1b1-a680-4870-89c5-3324d42da408";
+	public final static String R6b_DRVr_RAL_TDF_3TC = "3e7d018b-9669-4f48-9298-d7c633f86b0f";
+	public final static String R6c_DRVr_DTG_AZT_3TC = "5c8d27eb-4642-4c6f-b131-1182df8d1276";
+	public final static String R6d_DRVr_DTG_TDF_3TC = "ee226f28-5e37-4c40-a8e0-47dfade32712";
+
+	public final static String R6f_DRVr_DTG_ABC_3TC = "e3eb6ae1-fd70-47b2-b1a6-702fae87b061";
+	public final static String R6g_DRVr_ABC_3TC_EFV = "a270c994-8182-4204-9a1e-c7aa78a43b23";
+	public final static String R6h_DRVr_AZT_3TC_EFV = "f0724e33-ca6e-415e-b385-91d53e125cfb";
+
 }	
