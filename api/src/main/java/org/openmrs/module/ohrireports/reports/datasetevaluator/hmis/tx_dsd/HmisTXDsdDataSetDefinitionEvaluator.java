831c6061363bcf163efb1433fa3af6697abd203b
LijKal <getkalnow@gmail.com>
2023-06-20 21:13:02 +0300

    hmis tx dsd updated

--- api/src/main/java/org/openmrs/module/ohrireports/reports/datasetevaluator/hmis/tx_dsd/HmisTXDsdDataSetDefinitionEvaluator.java
+++ api/src/main/java/org/openmrs/module/ohrireports/reports/datasetevaluator/hmis/tx_dsd/HmisTXDsdDataSetDefinitionEvaluator.java
@@ -1,14 +1,10 @@
 package org.openmrs.module.ohrireports.reports.datasetevaluator.hmis.tx_dsd;
 
-import static org.openmrs.module.ohrireports.OHRIReportsConstants.ALIVE;
-import static org.openmrs.module.ohrireports.OHRIReportsConstants.FOLLOW_UP_STATUS;
-import static org.openmrs.module.ohrireports.OHRIReportsConstants.RESTART;
-import static org.openmrs.module.ohrireports.OHRIReportsConstants.TREATMENT_END_DATE;
+import static org.openmrs.module.ohrireports.OHRIReportsConstants.*;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Objects;
 
 import org.openmrs.Obs;
 import org.openmrs.annotation.Handler;
@@ -16,8 +12,9 @@ import org.openmrs.api.ConceptService;
 import org.openmrs.module.ohrireports.reports.datasetdefinition.hmis.tx_dsd.HmisTXDsdDataSetDefinition;
 import org.openmrs.module.reporting.dataset.DataSet;
 import org.openmrs.module.reporting.dataset.DataSetColumn;
-import org.openmrs.module.reporting.dataset.DataSetRow;
-import org.openmrs.module.reporting.dataset.SimpleDataSet;
+
+import org.openmrs.module.reporting.dataset.MapDataSet;
+
 import org.openmrs.module.reporting.dataset.definition.DataSetDefinition;
 import org.openmrs.module.reporting.dataset.definition.evaluator.DataSetEvaluator;
 import org.openmrs.module.reporting.evaluation.EvaluationContext;
@@ -48,102 +45,113 @@ public class HmisTXDsdDataSetDefinitionEvaluator implements DataSetEvaluator {
 
         hdsd = (HmisTXDsdDataSetDefinition) dataSetDefinition;
         context = evalContext;
-        SimpleDataSet set = new SimpleDataSet(dataSetDefinition, evalContext);
+        
+        MapDataSet data = new MapDataSet(dataSetDefinition, evalContext);
+        data.addData(new DataSetColumn("HIV_TX_DSD","Proportion of PLHIV currently on differentiated service Delivery model (DSD)",String.class)
+		," ");
+        obses = getDSDModel(DSD_3MMD);
+        data.addData(new DataSetColumn("HIV_TX_3MMD","Total number of clients on 3MMD",Integer.class)
+		,obses.size());
+        buildDataSet(data,1,50,"HIV_TX_3MMD");
 
-        DataSetRow femaleDateSet = new DataSetRow();
-        obses = LoadObs("F");
-        femaleDateSet.addColumnValue(new DataSetColumn("FineByAgeAndSexData", "Gender",
-                Integer.class), "Female");
-        femaleDateSet.addColumnValue(new DataSetColumn("unkownAge", "Unkown Age", Integer.class),
-        getUnknownAgeByGender());
-
-        femaleDateSet.addColumnValue(new DataSetColumn("<1", "Below One (<1)", Integer.class),
-        getEnrolledByAgeAndGender(0, 1));
-
-        buildDataSet(femaleDateSet, "F");
-
-
-
-        set.addRow(femaleDateSet);
-        obses = LoadObs("M");
-        DataSetRow maleDataSet = new DataSetRow();
-        maleDataSet.addColumnValue(new DataSetColumn("FineByAgeAndSexData", "Gender",
-                Integer.class), "Male");
-        maleDataSet.addColumnValue(new DataSetColumn("unkownAge", "Unkown Age", Integer.class),
-        getUnknownAgeByGender());
-        maleDataSet.addColumnValue(new DataSetColumn("<1", "Below One (<1)", Integer.class),
-                getEnrolledByAgeAndGender(0, 1));
-
-        buildDataSet(maleDataSet, "M");
-
-        set.addRow(maleDataSet);
-        return set;
+        obses = getDSDModel(DSD_6MMD);
+        data.addData(new DataSetColumn("HIV_TX_ASM","Total number of clients on ASM(6MMD)",Integer.class)
+		,obses.size());
+        buildDataSet(data,1,50,"HIV_TX_ASM");
+        
+        
+        return data;
     }
 
-    private void buildDataSet(DataSetRow dataSet, String gender) {
+    private void buildDataSet(MapDataSet data, Integer min_age, Integer max_age, String name) {
+        Integer counter = 0;
+        if (min_age==1){
+            counter+=1;
+            data.addData(new DataSetColumn(name+"."+counter,"< 1 year, Male",Integer.class),getdsdByAgeAndGender(0,min_age,Gender.Male));
+            counter+=1;
+            data.addData(new DataSetColumn(name+"."+counter,"< 1 year, Female",Integer.class),getdsdByAgeAndGender(0,min_age,Gender.Female));
+            getdsdByAgeAndGender(0,min_age,Gender.Female);
         minCount = 1;
         maxCount = 4;
-        while (minCount <= 65) {
-            if (minCount == 65) {
-                dataSet.addColumnValue(new DataSetColumn("65+", "65+", Integer.class),
-                        getEnrolledByAgeAndGender(65, 200));
+        }
+        else{
+            minCount = 15;
+            maxCount = 19;
+        }
+        while (minCount <= max_age) {
+            counter+=1;
+            if (minCount == max_age) {
+                data.addData(new DataSetColumn(name+"."+counter," >= "+max_age+" years, Male",Integer.class),getdsdByAgeAndGender(minCount,150,Gender.Male));
+                counter+=1;
+                data.addData(new DataSetColumn(name+"."+counter," >= "+max_age+" years, Female",Integer.class),getdsdByAgeAndGender(minCount,150,Gender.Female));
+			
+                
             } else {
-                dataSet.addColumnValue(
-                        new DataSetColumn(minCount + "-" + maxCount, minCount + "-" + maxCount, Integer.class),
-                        getEnrolledByAgeAndGender(minCount, maxCount));
+                data.addData(new DataSetColumn(name+"."+counter,minCount+" - "+maxCount+" years, Male",Integer.class),getdsdByAgeAndGender(minCount,maxCount,Gender.Male));
+                counter+=1;
+                data.addData(new DataSetColumn(name+"."+counter,minCount+" - "+maxCount+" years, Female",Integer.class),getdsdByAgeAndGender(minCount,maxCount,Gender.Female));
+
             }
 
             minCount=1+maxCount;
             maxCount = minCount + 4;
         }
     }
-    private int getUnknownAgeByGender(){
-        int count=0;
-        for (Obs obs : obses) {   
-            if ( Objects.isNull(obs.getPerson().getAge())|| obs.getPerson().getAge()==0) {
-                count++;
-            }
-        }
-
-        return count;
-    }
-
-    private int getEnrolledByAgeAndGender(int min, int max) {
-        int count =0;
-    //    List<Integer> persoIntegers = new ArrayList<>();
-        for (Obs obs : obses) {
-            // if (persoIntegers.contains(obs.getPersonId()))
-            //     continue;
-            
-            if (obs.getPerson().getAge() >= min && obs.getPerson().getAge() <= max) {
-                count++;
-            }
-
-            // persoIntegers.add(obs.getPersonId());
-        }
-
-        // clearCountedPerson(persoIntegers);
-        return count;
-    }
-
-    private void clearCountedPerson(List<Integer> personIds) {
-        for (int pId : personIds) {
-            obses.removeIf(p->p.getPersonId().equals(pId));
-        }
-    }
-    private List<Obs> LoadObs(String gender) {
-
-		List<Integer> patientsId = getListOfALiveORRestartPatientObservertions(gender);
+    
+    private Integer getdsdByAgeAndGender(int minAge, int maxAge, Gender gender) {
+        int _age = 0;
 		List<Integer> patients = new ArrayList<>();
-		List<Obs> localObs = new ArrayList<>();
+		String _gender=gender.equals(Gender.Female)?"f":"m";
+		if (maxAge > 1){
+				maxAge=maxAge+1;
+			}
+		for (Obs obs :obses) {
+			_age =obs.getPerson().getAge();
+			if (!patients.contains(obs.getPersonId()) 
+			 && (_age>=minAge && _age< maxAge)
+			 && (obs.getPerson().getGender().toLowerCase().equals(_gender))) {
+				
+				patients.add(obs.getPersonId());
+
+			}
+		}
+		return patients.size();
+    }
+
+ 
+    private List<Obs> getDSDModel(String dsd_type){
+        List<Integer> tx_curr = LoadObs();
+        List<Obs> patient_dsd = new ArrayList<>();
+        if (tx_curr == null || tx_curr.size() == 0)
+			return patient_dsd;
+        HqlQueryBuilder queryBuilder = new HqlQueryBuilder();
+
+		queryBuilder.select("obs")
+				.from(Obs.class, "obs")
+				.whereEqual("obs.encounter.encounterType", hdsd.getEncounterType())
+                .and()
+				.whereEqual("obs.concept", conceptService.getConceptByUuid(DSD_CATEGORY))
+				.and()
+                .whereIdIn("obs.personId", tx_curr)
+                .and()
+				.whereEqual("obs.valueCoded", conceptService.getConceptByUuid(dsd_type))
+				.and().whereLess("obs.obsDatetime", hdsd.getEndDate());
+		queryBuilder.orderDesc("obs.personId,obs.obsDatetime");
+        
+        return patient_dsd;
+
+    }
+    private List<Integer> LoadObs() {
+
+		List<Integer> patientsId = getListOfALiveORRestartPatientObservertions();
+		List<Integer> patients = new ArrayList<>();
 		if (patientsId == null || patientsId.size() == 0)
-			return localObs;
+			return patients;
 		HqlQueryBuilder queryBuilder = new HqlQueryBuilder();
 		queryBuilder.select("obs");
 		queryBuilder.from(Obs.class, "obs")
 				.whereEqual("obs.encounter.encounterType", hdsd.getEncounterType())
-				.and()
-                .whereEqual("obs.person.gender", gender)
+
                 .and()
 				.whereEqual("obs.concept", conceptService.getConceptByUuid(TREATMENT_END_DATE))
 				.and()
@@ -156,13 +164,13 @@ public class HmisTXDsdDataSetDefinitionEvaluator implements DataSetEvaluator {
 					if(!patients.contains(obs.getPersonId()))
 					  {
 						patients.add(obs.getPersonId());
-						localObs.add(obs);
+						
 					  }
 				}
 		
-		return localObs;
+		return patients;
 	}
-    private List<Integer> getListOfALiveORRestartPatientObservertions(String gender) {
+    private List<Integer> getListOfALiveORRestartPatientObservertions() {
 
 		List<Integer> uniqiObs = new ArrayList<>();
 		HqlQueryBuilder queryBuilder = new HqlQueryBuilder();
@@ -170,8 +178,6 @@ public class HmisTXDsdDataSetDefinitionEvaluator implements DataSetEvaluator {
 		queryBuilder.select("obs")
 				.from(Obs.class, "obs")
 				.whereEqual("obs.encounter.encounterType", hdsd.getEncounterType())
-				.and()
-                .whereEqual("obs.person.gender", gender)
                 .and()
 				.whereEqual("obs.concept", conceptService.getConceptByUuid(FOLLOW_UP_STATUS))
 				.and()
@@ -193,4 +199,9 @@ public class HmisTXDsdDataSetDefinitionEvaluator implements DataSetEvaluator {
 
 
 
+}
+
+enum Gender {
+	Female,
+	Male
 }
