4c0613a778e5507a543f2abb1fc18d11bd8ba4b5
abdisha <asresabdi@gmail.com>
2023-06-23 13:46:13 +0300

    datasetdefinition updated

--- api/src/main/java/org/openmrs/module/ohrireports/reports/hmis/HMISReport.java
+++ api/src/main/java/org/openmrs/module/ohrireports/reports/hmis/HMISReport.java
@@ -25,22 +25,22 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class HMISReport implements ReportManager {
-	
+
 	@Override
 	public String getUuid() {
 		return "4bf142d2-f92c-4a9e-9368-55801e9c025d";
 	}
-	
+
 	@Override
 	public String getName() {
 		return HMIS_REPORT + "-HMIS/DHIS";
 	}
-	
+
 	@Override
 	public String getDescription() {
 		return "06 - HIV | Hospital, Health center, Clinic | Monthly (Federal Ministry Of Health)";
 	}
-	
+
 	@Override
 	public List<Parameter> getParameters() {
 		Parameter startDate = new Parameter("startDate", "Start Date", Date.class);
@@ -53,7 +53,7 @@ public class HMISReport implements ReportManager {
 		endDateGC.setRequired(false);
 		return Arrays.asList(startDate, startDateGC, endDate, endDateGC);
 	}
-	
+
 	@Override
 	public ReportDefinition constructReportDefinition() {
 		ReportDefinition reportDefinition = new ReportDefinition();
@@ -63,65 +63,71 @@ public class HMISReport implements ReportManager {
 		reportDefinition.setParameters(getParameters());
 		HmisTXCurrDataSetDefinition aDefinition = new HmisTXCurrDataSetDefinition();
 		aDefinition.addParameters(getParameters());
-		aDefinition.setEncounterType(Context.getEncounterService().getEncounterTypeByUuid(HTS_FOLLOW_UP_ENCOUNTER_TYPE));
+		aDefinition
+				.setEncounterType(Context.getEncounterService().getEncounterTypeByUuid(HTS_FOLLOW_UP_ENCOUNTER_TYPE));
 		aDefinition.setDescription("06 - HIV | Hospital, Health center, Clinic | Monthly (Federal Ministry Of Health)");
 		reportDefinition.addDataSetDefinition(
-		    "HMIS:06 - HIV | Hospital, Health center, Clinic | Monthly (Federal Ministry Of Health)",
-		    map(aDefinition, "startDate=${startDateGC},endDate=${endDateGC}"));
-		
+				"HMIS:06 - HIV | Hospital, Health center, Clinic | Monthly (Federal Ministry Of Health)",
+				map(aDefinition, "startDate=${startDateGC},endDate=${endDateGC}"));
+
 		HIVTXNewDatasetDefinition txNewDataset = new HIVTXNewDatasetDefinition();
 		txNewDataset.setParameters(getParameters());
-		reportDefinition.addDataSetDefinition("HMIS:Number of adults and children with HIV infection newly started on ART",
-		    map(txNewDataset, "startDate=${startDateGC},endDate=${endDateGC}"));
-		
+		reportDefinition.addDataSetDefinition(
+				"HMIS:Number of adults and children with HIV infection newly started on ART",
+				map(txNewDataset, "startDate=${startDateGC},endDate=${endDateGC}"));
+
 		HIVARTRETDatasetDefinition hivArtRetDatasetDefinition = new HIVARTRETDatasetDefinition();
 		hivArtRetDatasetDefinition.addParameters(getParameters());
 		reportDefinition.addDataSetDefinition(
-		    "HMIS:Number of adults and children who are still on treatment at 12 months after\n" + //
-		            "initiating ART", map(hivArtRetDatasetDefinition, "startDate=${startDateGC},endDate=${endDateGC}"));
-		
+				"HMIS:Number of adults and children who are still on treatment at 12 months after\n" + //
+						"initiating ART",
+				map(hivArtRetDatasetDefinition, "startDate=${startDateGC},endDate=${endDateGC}"));
+
 		HIVLinkageNewCtDatasetDefinition linkageNewDataset = new HIVLinkageNewCtDatasetDefinition();
 		linkageNewDataset.setParameters(getParameters());
 		reportDefinition.addDataSetDefinition(
-		    "HMIS:Linkage outcome of newly identified Hiv positive individuals in the reporting period",
-		    map(linkageNewDataset, "startDate=${startDateGC},endDate=${endDateGC}"));
-		
+				"HMIS:Linkage outcome of newly identified Hiv positive individuals in the reporting period",
+				map(linkageNewDataset, "startDate=${startDateGC},endDate=${endDateGC}"));
+
 		HivPvlsDatasetDefinition hivPvlsDataset = new HivPvlsDatasetDefinition();
 		hivPvlsDataset.setParameters(getParameters());
 		hivPvlsDataset.setType(HivPvlsType.TESTED);
 		hivPvlsDataset.setPrefix(".1");
 		hivPvlsDataset
-		        .setDescription("Number of adult and pediatric ART patients for whom viral load test result received in the reporting period (with in the past 12 months)");
+				.setDescription(
+						"Number of adult and pediatric ART patients for whom viral load test result received in the reporting period (with in the past 12 months)");
 		reportDefinition
-		        .addDataSetDefinition(
-		            "HMIS:Viral load Suppression (Percentage of ART clients with a suppressed viral load among those with a viral load test at 12 month in the reporting period)",
-		            map(hivPvlsDataset, "startDate=${startDateGC},endDate=${endDateGC}"));
-		
+				.addDataSetDefinition(
+						"HMIS:Viral load Suppression (Percentage of ART clients with a suppressed viral load among those with a viral load test at 12 month in the reporting period)",
+						map(hivPvlsDataset, "startDate=${startDateGC},endDate=${endDateGC}"));
+
 		HivPvlsDatasetDefinition hivPvlsUnDataset = new HivPvlsDatasetDefinition();
 		hivPvlsUnDataset.setParameters(getParameters());
 		hivPvlsUnDataset.setType(HivPvlsType.SUPPRESSED);
 		hivPvlsUnDataset.setPrefix("_UN");
 		hivPvlsUnDataset
-		        .setDescription("Total number of adult and paediatric ART patients with an undetectable viral load(<50 copies/ml) in the reporting period  (with in the past 12 months)");
+				.setDescription(
+						"Total number of adult and paediatric ART patients with an undetectable viral load(<50 copies/ml) in the reporting period  (with in the past 12 months)");
 		reportDefinition
-		        .addDataSetDefinition(
-		            "HMIS:Viral load Suppression (Percentage of ART clients with a suppressed viral load among those with a viral load test at 12 month in the reporting period)",
-		            map(hivPvlsUnDataset, "startDate=${startDateGC},endDate=${endDateGC}"));
-		
+				.addDataSetDefinition(
+						"HMIS:Viral load Suppression (Percentage of ART clients with a suppressed viral load among those with a viral load test at 12 month in the reporting period)",
+						map(hivPvlsUnDataset, "startDate=${startDateGC},endDate=${endDateGC}"));
+
 		HivPvlsDatasetDefinition hivPvlsLvDataset = new HivPvlsDatasetDefinition();
 		hivPvlsLvDataset.setParameters(getParameters());
 		hivPvlsLvDataset.setType(HivPvlsType.LOW_LEVEL_LIVERMIA);
 		hivPvlsLvDataset.setPrefix("_LV");
 		hivPvlsLvDataset
-		        .setDescription("Total number of adult and paediatric ART patients with low level viremia (50 -1000 copies/ml) in the reporting period  (with in the past 12 months)");
+				.setDescription(
+						"Total number of adult and paediatric ART patients with low level viremia (50 -1000 copies/ml) in the reporting period  (with in the past 12 months)");
 		reportDefinition
-		        .addDataSetDefinition(
-		            "HMIS:Viral load Suppression (Percentage of ART clients with a suppressed viral load among those with a viral load test at 12 month in the reporting period)",
-		            map(hivPvlsLvDataset, "startDate=${startDateGC},endDate=${endDateGC}"));
-		
+				.addDataSetDefinition(
+						"HMIS:Viral load Suppression (Percentage of ART clients with a suppressed viral load among those with a viral load test at 12 month in the reporting period)",
+						map(hivPvlsLvDataset, "startDate=${startDateGC},endDate=${endDateGC}"));
+
 		return reportDefinition;
 	}
-	
+
 	public static <T extends Parameterizable> Mapped<T> map(T parameterizable, String mappings) {
 		if (parameterizable == null) {
 			throw new IllegalArgumentException("Parameterizable cannot be null");
@@ -131,22 +137,23 @@ public class HMISReport implements ReportManager {
 		}
 		return new Mapped<T>(parameterizable, ParameterizableUtil.createParameterMappings(mappings));
 	}
-	
+
 	@Override
 	public List<ReportDesign> constructReportDesigns(ReportDefinition reportDefinition) {
-		ReportDesign design = ReportManagerUtil.createExcelDesign("d15829f9-ad58-4421-8d82-11dd80ffaeb2", reportDefinition);
-		
+		ReportDesign design = ReportManagerUtil.createExcelDesign("d15829f9-ad58-4421-8d82-11dd80ffaeb2",
+				reportDefinition);
+
 		return Arrays.asList(design);
 	}
-	
+
 	@Override
 	public List<ReportRequest> constructScheduledRequests(ReportDefinition reportDefinition) {
 		return null;
 	}
-	
+
 	@Override
 	public String getVersion() {
 		return REPORT_VERSION;
 	}
-	
+
 }
